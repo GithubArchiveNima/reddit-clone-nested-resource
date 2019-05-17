c65ad3242e67b1d8bfaff6877a47c53ccd7cf41f
Nima Boscarino <nboscari@sfu.ca>
2019-05-17 10:02:45 -0700

    seeds, plus format the index page to show data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+t = Topic.create! name: 'Food', description: 'mmm food', image: 'https://veann.com/wp-content/uploads/2017/12/shutterstock_641257942-1-750x500.jpg'
