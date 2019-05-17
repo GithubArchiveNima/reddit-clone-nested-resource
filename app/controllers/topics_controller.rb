ecc0f91bc1146e2733d404d2e674dcc286de353a
Nima Boscarino <nboscari@sfu.ca>
2019-05-17 09:36:53 -0700

    render hello! in index page for topics

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -1,6 +1,12 @@
 class TopicsController < ApplicationController
 
   def index
+    @message = 'Hello!'
+    # implied that we call render on index, with some vars
   end
-  
+
+  # app.get('/', (req, res) => {
+  #   res.render('index', {})
+  # })
+
 end
