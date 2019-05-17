cc41743d74b7b7395ffdcc02579b74c759b710b1
Nima Boscarino <nboscari@sfu.ca>
2019-05-17 10:26:51 -0700

    add more seeds for topics, do conversations routing + index

---
+++ app/controllers/conversations_controller.rb
@@ -0,0 +1,7 @@
+class ConversationsController < ApplicationController
+  def index
+    puts 'PARAMS!'
+    puts params[:topic_id] # req.params
+    @topic = Topic.find(params[:topic_id]) # dynamic to the URL
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -8,3 +8,6 @@
 
 
 t = Topic.create! name: 'Food', description: 'mmm food', image: 'https://veann.com/wp-content/uploads/2017/12/shutterstock_641257942-1-750x500.jpg'
+
+t = Topic.create! name: 'Sports', description: 'gotta love sports!', image: 'https://www.capebretonpost.com/media/photologue/photos/cache/CB-23112018-Winter-Sports-SUB_large.jpg'
+
